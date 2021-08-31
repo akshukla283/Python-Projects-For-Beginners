0b63c4bf787e8f1c8e73b5f9865cda0283ee1cc8
akshukla283 <54815947+akshukla283@users.noreply.github.com>
2021-08-31 11:29:20 +0530

    Add files via upload

---
+++ print_color_text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Ankit Shukla "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Ankit Shukla")
+print(Fore.RED + Back.GREEN+ "Hi My name is Ankit Shukla")
